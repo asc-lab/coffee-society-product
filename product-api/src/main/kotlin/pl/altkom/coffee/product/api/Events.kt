86f88386245bb778fd809f88fd6a1671a2693417
Piotr Gębala <ppgebala@gmail.com>
2019-01-09 10:38:42 +0100

    ProductPreparationCancelledEvent missing fields

--- product-api/src/main/kotlin/pl/altkom/coffee/product/api/Events.kt
+++ product-api/src/main/kotlin/pl/altkom/coffee/product/api/Events.kt
@@ -1,8 +1,17 @@
 package pl.altkom.coffee.product.api
 
 data class ProductPreparationRegisteredEvent(
-        val id: String, val productDefId: String, val productReceiverName: String, val productExecutorName: String)
+        val id: String,
+        val productDefId: String,
+        val productReceiverName: String,
+        val productExecutorName: String
+)
 
-data class ProductPreparationCancelledEvent(val id: String)
+data class ProductPreparationCancelledEvent(
+        val id: String,
+        val productDefId: String,
+        val productReceiverName: String,
+        val productExecutorName: String
+)
 
 data class ProductReceiverChangedEvent(val id: String, val newProductReceiverName: String)
